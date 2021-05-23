662a1b39179104765a3cf8a58d846ce0cf3f4634
Erin Jeter <ekjeter@gmail.com>
2021-05-23 19:02:43 -0400

    login and signup components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
-// import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import Sitebar from "./Navbar";
+import Auth from "./Auth/Auth";
+import PollIndex from "./Polls/PollIndex";
 
 function App() {
-  return <div className="App"></div>;
+  const [sessionToken, setSessionToken] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
+    }
+  }, []);
+
+  const updateToken = (newToken) => {
+    localStorage.setItem("token", newToken);
+    setSessionToken(newToken);
+    console.log(sessionToken);
+  };
+
+  const clearToken = () => {
+    localStorage.clear();
+    setSessionToken("");
+  };
+
+  const protectedViews = () => {
+    return sessionToken === localStorage.getItem("token") ? (
+      <PollIndex token={sessionToken} />
+    ) : (
+      <Auth updateToken={updateToken} />
+    );
+  };
+
+  return (
+    <div className="App">
+      <Sitebar clearToken={clearToken} />
+      {protectedViews()}
+    </div>
+  );
 }
 
 export default App;
