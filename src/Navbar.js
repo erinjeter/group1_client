a2ad5194e25893ba89b491af19234b34ab921815
Emily Scudero <emily@MSI.COM>
2021-06-02 14:32:36 -0400

    finished create component

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,7 +9,11 @@ import {
   NavItem,
   NavLink,
   Collapse,
+  Modal,
+  ModalHeader,
 } from "reactstrap";
+
+import AddPoll from "./Polls/AddPoll";
 
 const Sitebar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,14 +22,38 @@ const Sitebar = (props) => {
     setIsOpen(newIsOpen);
   };
 
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const setModalIsOpenToTrue = () => {
+    setModalIsOpen(true);
+  };
+
+  const setModalIsOpenToFalse = () => {
+    setModalIsOpen(false);
+  };
+
   return (
     <Navbar color="light" light expand="md">
       <NavbarBrand href="/">Pollster</NavbarBrand>
       <NavbarToggler onClick={toggle} />
       <Collapse isOpen={isOpen} navbar>
-        <Nav className="ml-auto" navbar>
-          <Button className="mr-5">Add Poll</Button>{" "}
-        </Nav>
+        {localStorage.role === "admin" ? (
+          <>
+            <Nav className="ml-auto" navbar>
+              <Button className="mr-5" onClick={setModalIsOpenToTrue}>
+                Add Poll
+              </Button>{" "}
+              <Modal isOpen={modalIsOpen}>
+                <Button onClick={setModalIsOpenToFalse}>x</Button>
+                <ModalHeader>Add Poll</ModalHeader>
+
+                <AddPoll />
+              </Modal>
+            </Nav>
+          </>
+        ) : (
+          <></>
+        )}
+
         <Nav className="ml-auto" navbar>
           <Button className="mr-5" onClick={props.clearToken}>
             Logout
--- src/Polls/AddPoll.js
+++ src/Polls/AddPoll.js
@@ -29,20 +29,26 @@ const AddPoll = (props) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log(
+      "pollData",
+      question,
+      response1,
+      response2,
+      response3,
+      response4
+    );
     fetch("http://localhost:3000/poll/create", {
       method: "POST",
       body: JSON.stringify({
-        poll: {
-          question: question,
-          response1: response1,
-          response2: response2,
-          response3: response3,
-          response4: response4,
-        },
+        question: question,
+        response1: response1,
+        response2: response2,
+        response3: response3,
+        response4: response4,
       }),
       headers: new Headers({
         "Content-Type": "application/json",
-        Authorization: props.token,
+        Authorization: localStorage.getItem("token"),
       }),
     })
       .then((res) => res.json())
@@ -59,15 +65,17 @@ const AddPoll = (props) => {
 
   return (
     <div className="main">
-      <h1 className="addHeader">Add Poll</h1>
-
+      {/* <Modal isOpen={modal} toggle={toggle} className={className}>
+        <ModalHeader toggle={toggle}>Add Poll</ModalHeader>
+        <ModalBody> */}
       <Form onSubmit={handleSubmit}>
         <FormGroup>
           <Label for="question">Question</Label>
           <Input
             type="text"
             name="question"
-            id="question"
+            placeholder="poll question"
+            onChange={(e) => setQuestion(e.target.value)}
             placeholder="type a question"
           />
         </FormGroup>
@@ -76,7 +84,7 @@ const AddPoll = (props) => {
           <Input
             type="text"
             name="response1"
-            id="response1"
+            onChange={(e) => setResponse1(e.target.value)}
             placeholder="poll response"
           />
         </FormGroup>
@@ -85,7 +93,7 @@ const AddPoll = (props) => {
           <Input
             type="text"
             name="response2"
-            id="response2"
+            onChange={(e) => setResponse2(e.target.value)}
             placeholder="poll response"
           />
         </FormGroup>
@@ -94,36 +102,28 @@ const AddPoll = (props) => {
           <Input
             type="text"
             name="response3"
-            id="response3"
+            onChange={(e) => setResponse3(e.target.value)}
             placeholder="poll response"
           />
         </FormGroup>
         <FormGroup>
           <Label for="response4">Response 4</Label>
           <Input
-            type="text"
             name="response4"
-            id="response4"
+            placeholder="poll response"
+            onChange={(e) => setResponse4(e.target.value)}
             placeholder="poll response"
           />
         </FormGroup>
-        <Button onClick={reload}>Submit</Button>
+        <Button>Submit</Button>
       </Form>
-
-      <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Success!</ModalHeader>
-        <ModalBody>
-          {"The following poll has been added:"}
-          <br />
-
-          <br />
-        </ModalBody>
+      {/* </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={reload}>
             Add Another
           </Button>
         </ModalFooter>
-      </Modal>
+      </Modal> */}
     </div>
   );
 };
